a147553008becd740c63733a1f81c9621c48ab49
Eyal Ben Ivri <eyalbenivri@gmail.com>
2017-05-26 16:08:30 +0200

    made generic base job launcher, in preperation for yarn support

--- leader/src/main/scala/io/shinto/amaterasu/leader/mesos/MesosJobLauncher.scala
+++ leader/src/main/scala/io/shinto/amaterasu/leader/mesos/MesosJobLauncher.scala
@@ -1,12 +1,8 @@
 package io.shinto.amaterasu.leader.mesos
 
-import java.io.FileInputStream
-import java.nio.file.Paths
-
-import io.shinto.amaterasu.common.logging.Logging
 import io.shinto.amaterasu.common.configuration.ClusterConfig
 import io.shinto.amaterasu.leader.mesos.schedulers.JobScheduler
-import io.shinto.amaterasu.leader.utilities.Args
+import io.shinto.amaterasu.leader.utilities.{Args, BaseJobLauncher}
 import org.apache.mesos.Protos.FrameworkID
 import org.apache.mesos.{MesosSchedulerDriver, Protos}
 
@@ -14,23 +10,9 @@ import org.apache.mesos.{MesosSchedulerDriver, Protos}
   * The JobLauncher allows the execution of a single job, without creating a full
   * Amaterasu cluster (no cluster scheduler).
   */
-object JobLauncher extends App with Logging {
-  val parser = Args.getParser
-  parser.parse(args, Args()) match {
+object MesosJobLauncher extends BaseJobLauncher {
 
-    case Some(arguments: Args) =>
-
-      val config = ClusterConfig(new FileInputStream(s"${arguments.home}/amaterasu.properties"))
-      val resume = arguments.jobId != null
-
-      execute(arguments, config, resume)
-
-    case None =>
-    // arguments are bad, error message will have been displayed
-  }
-
-
-  def execute(arguments: Args, config: ClusterConfig, resume: Boolean): Unit = {
+  override def run(arguments: Args, config: ClusterConfig, resume: Boolean): Unit = {
     val frameworkBuilder = Protos.FrameworkInfo.newBuilder()
       .setName(s"${arguments.name} - Amaterasu Job")
       .setFailoverTimeout(config.timeout)
